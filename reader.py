ca7f136e2bbf4418b5f5574377b4174027c1b15f
davidjjeong <jaewoong.jeong@duke.edu>
2023-06-15 10:13:18 -0400

    Added reader to ignore empty lines

--- reader.py
+++ reader.py
@@ -10,18 +10,19 @@ def readFile(filepath):
     with open(filepath) as f:
         idx = 0
         for line in f:
-            event = EventData(idx)
-            tuples = ast.literal_eval(line)
-            num_layer = 0
+            if not line.strip():
+                event = EventData(idx)
+                tuples = ast.literal_eval(line)
+                num_layer = 0
 
-            for tuple in tuples:
-                newSpacePoint = SpacePoint(tuple[0], tuple[1], tuple[2], tuple[3])
-                event.appendPoint(tuple[0], newSpacePoint)
-                num_layer = max(num_layer, tuple[0])
-            
-            event.retrieveNumLayer(num_layer)
-            events.append(event)
-            idx += 1
+                for tuple in tuples:
+                    newSpacePoint = SpacePoint(tuple[0], tuple[1], tuple[2], tuple[3])
+                    event.appendPoint(tuple[0], newSpacePoint)
+                    num_layer = max(num_layer, tuple[0])
+                
+                event.retrieveNumLayer(num_layer)
+                events.append(event)
+                idx += 1
     # print(f"Time Taken:{time.time() - start}")
 
     return events
